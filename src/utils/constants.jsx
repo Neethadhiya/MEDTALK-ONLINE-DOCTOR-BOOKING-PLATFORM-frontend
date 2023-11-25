474e657b0c3e725013c60054b401748a26652588
unknown <neethadhiya@gmail.com>
2023-11-25 15:39:29 +0530

    wallet changed

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
-export const baseUrl = "http://127.0.0.1:8000";
-// export const baseUrl = "https://medtalk.online";
+// export const baseUrl = "http://127.0.0.1:8000";
+export const baseUrl = "https://medtalk.online";
 
 export const appID = 59691986
 export const serverSecret = '86dae3a3df6c5856390f47d635310359' 
