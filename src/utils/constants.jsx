c262406698614582037f04ac0b4a7a603a3e3ceb
unknown <neethadhiya@gmail.com>
2023-12-21 19:04:54 +0530

    app id changedd

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
-export const baseUrl = "http://127.0.0.1:8000";
-// export const baseUrl = "https://medtalk.online";
+// export const baseUrl = "http://127.0.0.1:8000";
+export const baseUrl = "https://medtalk.online";
 
 export const appID = 229598602;
 export const serverSecret = "31a8ec6e00921bf995fafd7740d06a45";
