6041669e1b05dee737f450c55e8497761e97d359
Neetha PA <83082780+Neethadhiya@users.noreply.github.com>
2024-02-26 16:25:47 +0530

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
 // export const baseUrl = "http://127.0.0.1:8000";
 export const baseUrl = "https://medtalk.online";
 
-export const appID = 229598602;
-export const serverSecret = "31a8ec6e00921bf995fafd7740d06a45";
+export const appID = 136900107;
+export const serverSecret = "4fa54c5d4526b670a06fe2a70a9f0702";
